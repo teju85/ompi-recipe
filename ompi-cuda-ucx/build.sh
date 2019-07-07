8a2d6430e87a36acffbb7c46ef9321fb55af573d
Thejaswi Rao <rao.thejaswi@gmail.com>
2019-07-06 21:55:18 -0700

    fixes to with-ucx build

--- ompi-cuda-ucx/build.sh
+++ ompi-cuda-ucx/build.sh
@@ -2,7 +2,16 @@ export CC=`basename $CC`
 export CXX=`basename $CXX`
 export LIBRARY_PATH=$PREFIX/lib
 
-pushd ompi && \
+export LDFLAGS="$LDFLAGS -L/usr/lib/x86_64-linux-gnu"
+
+pushd ucx && \
+    ./contrib/configure-release --prefix=$PREFIX \
+                                --disable-numa \
+                                --with-cuda=/usr/local/cuda && \
+    make -j${CPU_COUNT} && \
+    make install && \
+    popd && \
+    pushd ompi && \
     ./configure --prefix=$PREFIX \
                 --disable-dependency-tracking \
                 --disable-mpi-fortran \
@@ -10,7 +19,7 @@ pushd ompi && \
                 --disable-wrapper-runpath \
                 --with-cuda \
                 --with-sge \
-                --with-ucx \
+                --with-ucx=$PREFIX \
                 --with-wrapper-cflags="-I$PREFIX/include" \
                 --with-wrapper-cxxflags="-I$PREFIX/include" \
                 --with-wrapper-ldflags="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib" && \
